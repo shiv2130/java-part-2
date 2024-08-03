8170d3504796abbafc99790e21a1c44a599b71c9
shiv2130 <shivanahuomili@gmail.com>
2024-08-03 20:38:43 +0530

    learned how to define arrays

--- time.java
+++ time.java
@@ -1,7 +1,17 @@
 
 public class time {
  public static void main(String[] args) {
-     System.out.println("Hello!");
+     int[] marks = {93, 98, 99}; 
+    //  marks[0] = 90;
+    //  marks[1] = 98;
+    //  marks[2] = 100;
+    //  System.out.println(marks);
+    //  System.out.println(marks[0]);
+    //  System.out.println(marks[1]);
+    //  System.out.println(marks[2]);
+     for(int i =0; i<3; i++){
+        System.out.println(marks[i]);
+     }
  }
     
 }
