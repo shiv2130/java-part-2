94122be7e85b1fced737d96c102129c47db1283e
shiv2130 <shivanahuomili@gmail.com>
2024-08-12 09:20:29 +0530

    Working on sentence and strings

---
+++ stringsent.java
@@ -0,0 +1,7 @@
+public class stringsent {
+    public static void main(String[] args) {
+        String sentence = "I am Iron Man";
+        String name = sentence.substring(10);
+        System.out.println(name);
+    }
+}
