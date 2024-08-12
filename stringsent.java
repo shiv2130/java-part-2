523f00fb4bdb29aab5db8d0ec9bcfbbbd358ad5d
shiv2130 <shivanahuomili@gmail.com>
2024-08-12 09:33:56 +0530

    Added parsInt Method and to string method to string class

--- stringsent.java
+++ stringsent.java
@@ -1,7 +1,17 @@
 public class stringsent {
     public static void main(String[] args) {
-        String sentence = "I am Iron Man";
-        String name = sentence.substring(10);
-        System.out.println(name);
+        // String sentence = "I am Iron Man";
+        // String name = sentence.substring(10);
+        // System.out.println(name);
+
+        //parsInt method
+        String str = "123";
+        int number  = Integer.parseInt(str);
+        System.out.println(number);
+
+        //to string method to string class
+        int num = 123;
+        String strE = Integer.toString(num);
+        System.out.println(strE.length());
     }
 }
