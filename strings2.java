accb136df69a6df2ee9815d46567fb887e13a58c
shiv2130 <shivanahuomili@gmail.com>
2024-08-13 15:54:19 +0530

    Used delete method

--- strings2.java
+++ strings2.java
@@ -4,14 +4,24 @@ public class strings2 {
         StringBuilder sb = new StringBuilder("Shivanshu");
         System.out.println(sb);
 
-        // //char at index 0
-        // System.out.println(sb.charAt(0));
+        //char at index 0
+        System.out.println(sb.charAt(0));
 
-        // //set char at index 1
-        // sb.setCharAt(0 , 'H' );
-        // System.out.println(sb);
+        //set char at index 1
+
+        
+        sb.setCharAt(0 , 'H' );
+        System.out.println(sb);
+
+
+        //replacing character at any index with insert method
 
         sb.insert(3, 'p');
+        System.out.println(sb);
+
+        //deleting extra characters
+
+        sb.delete(1, 2);
         System.out.println(sb);
 
 
