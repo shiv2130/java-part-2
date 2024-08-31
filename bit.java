e9df9c342ae7bc138a512f3398df69e0a68c938c
shiv2130 <shivanahuomili@gmail.com>
2024-08-31 16:52:15 +0530

    Bitmasking or bit manipulation or Setbit

---
+++ bit.java
@@ -0,0 +1,12 @@
+public class bit {
+    public static void main(String[] args) {
+        int n = 5;
+        int pos = 3;
+        int bitMask = 1<<pos;
+        if((bitMask&n) == 0){
+            System.out.println("Bit was zero");
+        }else{
+            System.out.println("bit was one");
+        }
+    }
+}
