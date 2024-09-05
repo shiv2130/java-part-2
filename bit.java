2f74c27fc2844e9f7ccaefe6985f7699dabacc1b
shiv2130 <shivanahuomili@gmail.com>
2024-09-05 18:16:41 +0530

    trying clear bit

--- bit.java
+++ bit.java
@@ -1,27 +1,33 @@
-import java.util.*;
 public class bit {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int oper = sc.nextInt();
+        // Scanner sc = new Scanner(System.in);
+        // int oper = sc.nextInt();
         int n = 5;
-        int pos =  1;
+        int pos =  2;
         int bm = 1<<pos;
         // int bitMask = 1<<pos;
         // int notMask =  ~(bitMask);
         // int newNumber = notMask & n;
         // System.out.println(newNumber);
-        if(oper == 1){
-            // update bit to 1 else update bit to 0
+        // if(oper == 1){
+        //     // update bit to 1 else update bit to 0
            
-            int newNumber  = bm | n;
-            System.out.println(newNumber);
+        //     int newNumber  = bm | n;
+        //     System.out.println(newNumber);
 
-        }else{
-            int newBitMask = ~(bm);
-            int newNumber = newBitMask & n;
-            System.out.println(newNumber);
+        // }else{
+        //     int newBitMask = ~(bm);
+        //     int newNumber = newBitMask & n;
+        //     System.out.println(newNumber);
 
-        }
+        // }
 
+
+
+        //clear bit 
+        int newBitMask = ~(bm);
+        int newNumber = newBitMask & n;
+        System.out.println(newNumber);
+        
     }
 }
