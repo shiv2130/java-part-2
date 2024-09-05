f8cc426bab9b142c84fb91f664ebfb00b8838c00
shiv2130 <shivanahuomili@gmail.com>
2024-09-05 18:10:21 +0530

    Bit manipulation

--- bit.java
+++ bit.java
@@ -1,12 +1,27 @@
+import java.util.*;
 public class bit {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int oper = sc.nextInt();
         int n = 5;
-        int pos = 3;
-        int bitMask = 1<<pos;
-        if((bitMask&n) == 0){
-            System.out.println("Bit was zero");
+        int pos =  1;
+        int bm = 1<<pos;
+        // int bitMask = 1<<pos;
+        // int notMask =  ~(bitMask);
+        // int newNumber = notMask & n;
+        // System.out.println(newNumber);
+        if(oper == 1){
+            // update bit to 1 else update bit to 0
+           
+            int newNumber  = bm | n;
+            System.out.println(newNumber);
+
         }else{
-            System.out.println("bit was one");
+            int newBitMask = ~(bm);
+            int newNumber = newBitMask & n;
+            System.out.println(newNumber);
+
         }
+
     }
 }
